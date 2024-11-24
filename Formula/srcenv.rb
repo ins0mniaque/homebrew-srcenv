63e14ecb9eb4ab46c31dbf5d8bc0c48b5be74bd2
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-11-24 01:03:56 -0500

    Bump srcenv to 1.1.1

--- Formula/srcenv.rb
+++ Formula/srcenv.rb
@@ -1,10 +1,10 @@
 class Srcenv < Formula
   desc "A cross-shell tool for sourcing POSIX compliant .env scripts."
   homepage "https://github.com/ins0mniaque/srcenv"
-  url "https://github.com/ins0mniaque/srcenv/archive/refs/tags/v1.1.0.zip"
-  sha256 "3f1eee8c9b334a6ce193a6aabd25fb3a56597feb296bd70424b4ff1ae5a01484"
+  url "https://github.com/ins0mniaque/srcenv/archive/refs/tags/v1.1.1.zip"
+  sha256 "f3732b3cab130100e4fc5498db264e50517700f92264886eed83fe07d27f1f7c"
   license "MIT"
-  version "1.1.0"
+  version "1.1.1"
 
   depends_on "jq"
 
@@ -14,7 +14,7 @@ class Srcenv < Formula
   end
 
   test do
-    expected_version = "srcenv 1.1.0"
+    expected_version = "srcenv 1.1.1"
     actual_version = shell_output("#{bin}/srcenv --version").strip
     assert_match expected_version, actual_version
   end

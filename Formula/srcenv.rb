6cf6186dec71fb0ab0c736c691df8343135e0962
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-11-25 04:53:01 -0500

    Fix formula description

--- Formula/srcenv.rb
+++ Formula/srcenv.rb
@@ -1,10 +1,10 @@
 class Srcenv < Formula
-  desc "A cross-shell tool for sourcing POSIX compliant .env scripts."
+  desc "Cross-shell tool for sourcing POSIX compliant .env scripts"
   homepage "https://github.com/ins0mniaque/srcenv"
   url "https://github.com/ins0mniaque/srcenv/archive/refs/tags/v1.2.3.zip"
+  version "1.2.3"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
-  version "1.2.3"
 
   depends_on "jq"
 

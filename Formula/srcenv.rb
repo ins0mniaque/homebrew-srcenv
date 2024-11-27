c3454e035613cffeb9eea9f9889153d9c8d62720
ins0mniaque <206899+ins0mniaque@users.noreply.github.com>
2024-11-27 15:09:14 +0000

    Bump srcenv to 1.2.17

--- Formula/srcenv.rb
+++ Formula/srcenv.rb
@@ -1,9 +1,9 @@
 class Srcenv < Formula
   desc "Cross-shell tool for sourcing POSIX compliant .env scripts"
   homepage "https://github.com/ins0mniaque/srcenv"
-  url "https://github.com/ins0mniaque/srcenv/archive/refs/tags/v1.2.16.tar.gz"
-  version "1.2.16"
-  sha256 "92c5005ffa989f2fe938dfc1b24f595ebc2cd374a202714e67d11e72dfacb998"
+  url "https://github.com/ins0mniaque/srcenv/archive/refs/tags/v1.2.17.tar.gz"
+  version "1.2.17"
+  sha256 "7548067ae552272946ef8a27c5b8ca721d0432288d1bdbf7d4d9d968a7dd6e4b"
   license "MIT"
 
   depends_on "jq"
